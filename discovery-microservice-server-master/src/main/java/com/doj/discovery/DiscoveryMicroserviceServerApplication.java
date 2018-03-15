701b6d714b24bc7429ead048d6f5d327081269e8
Anbarasu Venkatachalam <anbarasuv@hcl.com>
2018-03-15 15:30:23 +0530

    Author details added

--- discovery-microservice-server-master/src/main/java/com/doj/discovery/DiscoveryMicroserviceServerApplication.java
+++ discovery-microservice-server-master/src/main/java/com/doj/discovery/DiscoveryMicroserviceServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author anbarasuv
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryMicroserviceServerApplication {
